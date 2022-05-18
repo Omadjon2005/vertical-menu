51d6a01586c5c0580fb82344a0c5de7a43e633f8
Omadjon <MaxkambayevOmadjon@gmail.com>
2022-05-18 18:01:15 +0500

    my commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let sidebar = document.querySelector('.sidebar');
+let btn = document.querySelector('#btn');
+let serachBtn = document.querySelector('.fa-magnifying-glass');
+btn.addEventListener('click',()=>{
+    sidebar.classList.toggle('active');
+})
+
+
+
+
+
+
+
+
